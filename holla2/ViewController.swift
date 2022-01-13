ec955573e77dbb1b778cbf6b7562a97339daf7a4
Adam <apple.teoron@gmail.com>
2022-01-13 17:20:50 -0500

    button

--- holla2/ViewController.swift
+++ holla2/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var toggleButton: UIButton!
+    @IBOutlet var hollaMiaLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        hollaMiaLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 15
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeButtonAction(_ sender: Any) {
+        if hollaMiaLabel.isHidden {
+            hollaMiaLabel.isHidden = false
+            toggleButton.setTitle("Hide Text", for: .normal)
+        } else {
+            hollaMiaLabel.isHidden = true
+            toggleButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 }
 
