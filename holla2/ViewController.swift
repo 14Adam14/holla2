84eb95f78c9f1bea0c9ccfa469e7d5c36680d603
Adam <apple.teoron@gmail.com>
2022-01-13 13:05:19 -0500

    Initial Commit

---
+++ holla2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  holla2
+//
+//  Created by Sergei on 1/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
